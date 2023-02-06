4fd99c158b2c9d255d1ad1a3fab432ac0fc4bbba
YU3727 <lacvert3727@naver.com>
2023-02-06 16:33:54 +0900

    e

--- src/main/java/com/pooh/s1/member/MemberDAO.java
+++ src/main/java/com/pooh/s1/member/MemberDAO.java
@@ -12,26 +12,30 @@ import com.pooh.s1.util.DBConnection;
 @Repository
 public class MemberDAO {
 //230202 8교시
+//230206 7교시 Mybatis를 이용하여 memberJoin 만들어보기	
 	
-	public int memberJoin(MemberDTO memberDTO) throws Exception{
-		
-		Connection connection = DBConnection.getConnection();
-		
-		String sql = "INSERT INTO MEMBER "
-				+ "VALUES(?, ?, ?, ?, ?)";
-		
-		PreparedStatement st = connection.prepareStatement(sql);
-		
-		st.setString(1, memberDTO.getID());
-		st.setString(2, memberDTO.getPW());
-		st.setString(3, memberDTO.getMemberName());
-		st.setString(4, memberDTO.getMemberPhone());
-		st.setString(5, memberDTO.getEmail());
-		
-		int result = st.executeUpdate();
-		
-		DBConnection.disConnection(st, connection);
-		
-		return result;	
-	}
+	
+	
+	//memberJoin
+//	public int memberJoin(MemberDTO memberDTO) throws Exception{
+//		
+//		Connection connection = DBConnection.getConnection();
+//		
+//		String sql = "INSERT INTO MEMBER "
+//				+ "VALUES(?, ?, ?, ?, ?)";
+//		
+//		PreparedStatement st = connection.prepareStatement(sql);
+//		
+//		st.setString(1, memberDTO.getID());
+//		st.setString(2, memberDTO.getPW());
+//		st.setString(3, memberDTO.getMemberName());
+//		st.setString(4, memberDTO.getMemberPhone());
+//		st.setString(5, memberDTO.getEmail());
+//		
+//		int result = st.executeUpdate();
+//		
+//		DBConnection.disConnection(st, connection);
+//		
+//		return result;	
+//	}
 }
